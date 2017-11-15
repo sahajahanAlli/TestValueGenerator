64fbe0629fb63a091e4fef1f503cf1adf117ceee
saha <saju.alli786@gmail.com>
2017-11-15 12:00:09 +0530

    second commit

--- src/com/random/value/RandomGeneratorClass.java
+++ src/com/random/value/RandomGeneratorClass.java
@@ -4,24 +4,6 @@ import java.util.ArrayList;
 
 public class RandomGeneratorClass {
 	
-	public void valueGenerator(){
-		
-		int[] asciiCharArray={65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122};
-		int[] singleNumbers ={0,1,2,3,4,5,6,7,8,9};
-		int[] specialChars={39,40,41,43,44,45,46,58};
-		
-		for(int i=0;i<asciiCharArray.length;i++){
-			System.out.print((char)asciiCharArray[i]);
-		}
-		
-		for(int i=0;i<singleNumbers.length;i++){
-			System.out.print(singleNumbers[i]);
-		}
-		
-		for(int i=0;i<specialChars.length;i++){
-			System.out.print((char)specialChars[i]);
-		}
-	}
 	
 	public ArrayList<String> singleCharValue(){
 		
